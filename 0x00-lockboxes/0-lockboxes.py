d3c79de5226d03169649ecd1cb5790f3db789e6f
AndZapata <andzapata514@gmail.com>
2020-04-19 01:39:57 +0000

    [Fix] check next value in box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,17 +3,17 @@
 
 def canUnlockAll(boxes):
     ''' Function that checks if all boxes are unlocked or not '''
-    check = []
-    check.insert(0, True)
-    for i in range(len(boxes) - 1):
-        before = i-1
-        after = i+1
-        if after in boxes[i]:
-            check.insert(i, True)
-        elif before in boxes[i]:
-            check.insert(i, True)
-        else:
-            check.insert(i, False)
+    check = [None] * len(boxes)
+    check[0] = True
+    for i in range(1, len(boxes)):
+        flag = False
+        for j in range(len(boxes)):
+            flag = i in boxes[j] and i != j
+            if flag != False:
+                check[i] = flag
+                break
+        if flag is False:
+            check[i] = flag
     if False in check:
         return False
     return True
