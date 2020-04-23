b3656fbbad16811622b22043b88c4d224f471844
AndZapata <andzapata514@gmail.com>
2020-04-23 19:27:09 +0000

    [Add] insert a number in a sorted linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert a new node in a sorted linked list
+ * @head: pointer to head of list
+ * @number: data to add in the linked list
+ * Return: New node
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+	listint_t *current, *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+
+	if (head == NULL || (*head)->n >= new_node->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	while (current->next && current->next->n < new_node->n)
+		current = current->next;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,5 @@
+# 0x01-insert_in_sorted_linked_list
+
+## Project from Holberton School
+
+Understanding how to introduce a node in a sorted linked list
