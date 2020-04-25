9caa1f26e95beb8eba6ee44c563d96dadbd3d13f
AndZapata <andzapata514@gmail.com>
2020-04-25 22:07:22 +0000

    [Fix] Null case

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -17,7 +17,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	new_node->n = number;
 
-	if (head == NULL || (*head)->n >= new_node->n)
+	if (*head == NULL || (*head)->n >= new_node->n)
 	{
 		new_node->next = *head;
 		*head = new_node;
