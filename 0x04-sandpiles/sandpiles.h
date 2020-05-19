6474d9bec435ccf9029b77f6e4fd94559ecf7cf7
AndZapata <andzapata514@gmail.com>
2020-05-19 02:06:57 +0000

    [Fix] header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -6,10 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void sum(int grid1[3][3], int grid2[3][3]);
 int check(int grid1[3][3]);
 void new_print_grid(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum(int grid1[3][3], int grid2[3][3]);
 void toppling(int grid1[3][3]);
 
 #endif /* _SANDPILES_H_ */
