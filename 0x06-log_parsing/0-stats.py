9960fa3c826c7baad4a27014ab6b896eae260f18
AndZapata <andzapata514@gmail.com>
2020-06-03 03:35:19 +0000

    [Fix] total size

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,7 +12,7 @@ try:
         code = stdin_list[-2]
         if code in status_code.keys():
             status_code[code] = status_code.get(code) + 1
-            total_size += int(stdin_list[-1])
+        total_size += int(stdin_list[-1])
         counter += 1
         if counter % 10 == 0:
             print('File size: {}'.format(total_size))
