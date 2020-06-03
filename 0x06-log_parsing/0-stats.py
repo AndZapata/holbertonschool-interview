a3ea2f4a3ddc855cb1d12ea23667d41bad10194b
AndZapata <andzapata514@gmail.com>
2020-06-03 03:41:24 +0000

    [Fix] format

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,19 +10,19 @@ try:
     for i in sys.stdin:
         stdin_list = i.split(" ")
         code = stdin_list[-2]
+        total_size += int(stdin_list[-1])
         if code in status_code.keys():
             status_code[code] = status_code.get(code) + 1
-        total_size += int(stdin_list[-1])
         counter += 1
         if counter % 10 == 0:
-            print('File size: {}'.format(total_size))
+            print('File size: {:d}'.format(total_size))
             for key, val in sorted(status_code.items()):
                 if val != 0:
-                    print('{}: {}'.format(key, val))
+                    print('{:s}: {:d}'.format(key, val))
 except Exception:
     pass
 finally:
-    print('File size: {}'.format(total_size))
+    print('File size: {:d}'.format(total_size))
     for key, val in sorted(status_code.items()):
         if val != 0:
-            print('{}: {}'.format(key, val))
+            print('{:s}: {:d}'.format(key, val))
