b26a84f5b32f5ab2a683253b8a7770b02a9810ce
AndZapata <andzapata514@gmail.com>
2020-06-03 04:47:15 +0000

    [Fix] check len of splited list

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,11 +9,12 @@ status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
 try:
     for i in sys.stdin:
         stdin_list = i.split(" ")
-        code = stdin_list[-2]
-        total_size += int(stdin_list[-1])
-        if code in status_code.keys():
-            status_code[code] = status_code.get(code) + 1
-        counter += 1
+        if len(stdin_list) > 2:
+            code = stdin_list[-2]
+            total_size += int(stdin_list[-1])
+            if code in status_code.keys():
+                status_code[code] = status_code.get(code) + 1
+            counter += 1
         if counter % 10 == 0:
             print('File size: {:d}'.format(total_size))
             for key, val in sorted(status_code.items()):
