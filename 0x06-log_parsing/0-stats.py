e09c2562b35d1b7d0c8ec5d406b58cbd55af2466
AndZapata <andzapata514@gmail.com>
2020-06-03 03:24:43 +0000

    [Fix] pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ import sys
 counter = 0
 total_size = 0
 status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
-                   '403': 0, '404': 0, '405': 0, '500': 0}
+               '403': 0, '404': 0, '405': 0, '500': 0}
 try:
     for i in sys.stdin:
         stdin_list = i.split(" ")
