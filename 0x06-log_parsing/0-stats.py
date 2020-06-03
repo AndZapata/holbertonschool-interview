61def83050642c0a79b69e9ea15bc90746135a73
AndZapata <andzapata514@gmail.com>
2020-06-03 03:21:36 +0000

    [Add] First commit

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+
+counter = 0
+total_size = 0
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+                   '403': 0, '404': 0, '405': 0, '500': 0}
+try:
+    for i in sys.stdin:
+        stdin_list = i.split(" ")
+        code = stdin_list[-2]
+        if code in status_code.keys():
+            status_code[code] = status_code.get(code) + 1
+            total_size += int(stdin_list[-1])
+        counter += 1
+        if counter % 10 == 0:
+            print('File size: {}'.format(total_size))
+            for key, val in sorted(status_code.items()):
+                if val != 0:
+                    print('{}: {}'.format(key, val))
+except Exception:
+    pass
+finally:
+    print('File size: {}'.format(total_size))
+    for key, val in sorted(status_code.items()):
+        if val != 0:
+            print('{}: {}'.format(key, val))
