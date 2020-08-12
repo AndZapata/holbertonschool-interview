462745c7747fa581a590304a8982ad5e90c41d33
andzapata <andzapata514@gmail.com>
2020-08-12 20:34:13 +0000

    [Add] skip list project

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,48 @@
+#include "search.h"
+
+/**
+ * linear_skip - Function to check a value in a linked list
+ * @list: Pointer to the head of the list
+ * @value: Value to be search
+ *
+ * Return: Pointer to the value or null if fail
+ **/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *next, *prev;
+	if (!list)
+		return (NULL);
+	next = list->express;
+	prev = list;
+	while (next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       next->index, next->n);
+		if (next->n >= value || !next->express)
+		{
+			if (!next->express && next->n < value)
+			{
+				prev = next;
+				while (next->next)
+					next = next->next;
+			}
+			printf("Value found between indexes [%lu] and [%lu]\n",
+			       prev->index, next->index);
+			while (prev)
+			{
+				printf("Value checked at index [%lu] = [%d]\n",
+				       prev->index, prev->n);
+				if (prev->n == value)
+					return (prev);
+				if (prev->n > value)
+					return (NULL);
+				prev = prev->next;
+			}
+			break;
+		}
+		prev = next;
+		next = next->express;
+	}
+	return (NULL);
+}
